610ef4cbf53bc3c50b5ba59b00de1e55d146136a
xu6148152 <xu6148152@gmail.com>
2019-12-28 17:26:49 +0800

    add whatsapp_demo

--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -1,6 +1,9 @@
 package io.flutter.plugins;
 
 import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugins.camera.CameraPlugin;
+import io.flutter.plugins.pathprovider.PathProviderPlugin;
+import com.tekartik.sqflite.SqflitePlugin;
 
 /**
  * Generated file. Do not edit.
@@ -10,6 +13,9 @@ public final class GeneratedPluginRegistrant {
     if (alreadyRegisteredWith(registry)) {
       return;
     }
+    CameraPlugin.registerWith(registry.registrarFor("io.flutter.plugins.camera.CameraPlugin"));
+    PathProviderPlugin.registerWith(registry.registrarFor("io.flutter.plugins.pathprovider.PathProviderPlugin"));
+    SqflitePlugin.registerWith(registry.registrarFor("com.tekartik.sqflite.SqflitePlugin"));
   }
 
   private static boolean alreadyRegisteredWith(PluginRegistry registry) {
---
+++ lib/whatsapp_demo/pages/call_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CallsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+        child: new Text("calls", style: new TextStyle(fontSize: 20.0)));
+  }
+}
